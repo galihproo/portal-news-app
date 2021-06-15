b37ab69b1245bc313d52a05995a7c54bca27f4bb
galihproo <galihprihananto29@gmail.com>
2021-06-15 18:09:49 +0700

    first commit

---
+++ src/constants/categories.js
@@ -0,0 +1,34 @@
+export const CATEGORIES = [
+    {
+        name: 'Apple',
+        slug: 'apple'
+    },
+    {
+        name: 'Life',
+        slug: 'life'
+    },
+    {
+        name: 'Financial',
+        slug: 'financial'
+    },
+    {
+        name: 'Technology',
+        slug: 'technology'
+    },
+    {
+        name: 'Google',
+        slug: 'google'
+    },
+    {
+        name: 'Education',
+        slug: 'education'
+    },
+    {
+        name: 'Hobby',
+        slug: 'hobby'
+    },
+    {
+        name: 'Sport',
+        slug: 'sport'
+    }
+]
